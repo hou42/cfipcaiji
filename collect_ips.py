9796d0b01d57640f3b7fb9a524d2e5f58410f7c8
hou42 <143340267+hou42@users.noreply.github.com>
2024-03-22 21:25:47 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz'
+        'https://download.parallels.com/desktop/v15/15.1.5-47309/ParallelsDesktop-15.1.5-47309.dmg'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -27,7 +27,7 @@ with open('ip.txt', 'w') as file:
         # 根据网站的不同结构找到包含IP地址的元素
         if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
             elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
+        elif url == 'https://download.parallels.com/desktop/v15/15.1.5-47309/ParallelsDesktop-15.1.5-47309.dmg':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
